c7640a094cd81ecc51b5c38c1ad0834b4ebb2f60
SuryaNair123 <94546915+SuryaNair123@users.noreply.github.com>
2021-11-17 13:25:13 -0500

    Update SpringMain.java

--- SpringAOPExample/src/main/java/com/journaldev/spring/main/SpringMain.java
+++ SpringAOPExample/src/main/java/com/journaldev/spring/main/SpringMain.java
@@ -11,7 +11,7 @@ public class SpringMain {
 		EmployeeService employeeService = ctx.getBean("employeeService", EmployeeService.class);
 		
 		System.out.println(employeeService.getEmployee().getName());
-		System.out.println("Testing repo changes");
+		System.out.println("Testing repo changes from repo");
 		
 		System.out.println("Testing from local to commit changes to repo");
 		
