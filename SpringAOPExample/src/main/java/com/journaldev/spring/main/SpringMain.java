f2cda0c5ecd043cd8e9da893819dc760c01f253e
SuryaNair123 <94546915+SuryaNair123@users.noreply.github.com>
2021-11-17 12:42:07 -0500

    Update SpringMain.java

--- SpringAOPExample/src/main/java/com/journaldev/spring/main/SpringMain.java
+++ SpringAOPExample/src/main/java/com/journaldev/spring/main/SpringMain.java
@@ -11,6 +11,8 @@ public class SpringMain {
 		EmployeeService employeeService = ctx.getBean("employeeService", EmployeeService.class);
 		
 		System.out.println(employeeService.getEmployee().getName());
+		System.out.println("Testing repo changes");
+		
 		
 		employeeService.getEmployee().setName("suria");
 		
